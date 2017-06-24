e40c2cdf5950e91d856747c469743d2c86ca9ae5
Jeff Knecht <jeff_knecht@yahoo.com>
2017-06-24 16:06:13 -0500

    remove unused import

--- src/main/java/SqliteDatabase.java
+++ src/main/java/SqliteDatabase.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Iterator;
 
 public class SqliteDatabase implements EventListener {
     private Connection conn;
